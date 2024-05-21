5ed89ab99538ef291ffa0d7f6132c97d956fc17c
rifai-rizqi3 <rnrifai12@gmail.com>
2024-05-21 12:53:09 +0700

    handle_fuction

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use postgres::{Client, NoTls};
 use postgres::Error as PostgresError;
+use std::f32::consts::E;
 use std::net::{TcpListener, TcpStream};
 use std::io::{Read, Write};
 use std::env;
@@ -47,6 +48,32 @@ fn main() {
         }
     }
 }
+
+// handle_client function
+fn handle_client(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    let mut request = String::new();
+
+    match stream.read(&mut buffer) {
+        Ok(size) => {
+            request.push_str(string::from_utf8_lossy(&buffer[..size]).as_ref());
+        
+            let (status_line, content) = match &*request {
+                r if request_with("POST /users") => handle_post_request(r),
+                r if request_with("GET /users/") => handle_get_request(r),
+                r if request_with("GET /users") => handle_get_all_request(),
+                r if request_with("PUT /users/") => handle_put_request(r),
+                r if request_with("DELETE /users/") => handle_delete_request(r),
+                _ => (NOT_FOUND, "Not Found".to_string()),
+            };
+            stream.write_all(format!("{}{}", status_line, content).as_bytes()).unwrap();
+        }
+        Err(e) => {
+            println!("Error: {}", e);
+        }
+    }
+}
+
 
 // set_database function
 fn set_database() -> Resul<(), PostgresError> {
