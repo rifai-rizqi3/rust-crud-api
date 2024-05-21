bfd249a95dbfc4a5d5a428f883eb2e767d393e27
rifai-rizqi3 <rnrifai12@gmail.com>
2024-05-21 14:42:55 +0700

    delete func

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+# Build Stage
+FROM rust:1.69-buster as builder
+
+WORKDIR /app
+
+# Accept the build argument
+ARG DATABASE_URL
+ENV DATABASE_URL=$DATABASE_URL
+COPY . .
+RUN cargo build --release
+
+# Production Stage
+FROM debian:buster-slim
+WORKDIR /usr/local/bin
+COPY --from=builder /app/target/release/rust-crud-api .
+CMD [ "./rust-crud-api" ]
--- src/main.rs
+++ src/main.rs
@@ -74,6 +74,98 @@ fn handle_client(mut stream: TcpStream) {
     }
 }
 
+// Controllers
+
+// handle_post_request function
+fn handle_post_request(request: &str) -> (String, String) {
+    match (get_user_request_body(request), Client::connect(DB_URL, NoTls)) {
+        (Ok(user), Ok(mut client)) => {
+            client.execute(
+                "INSERT INTO users (name, email) VALUES ($1, $2)",
+                &[&user.name, &user.email]
+            ).unwrap();
+
+            (OK_RESPONSE.to_string(), "User created".to_string())
+        }
+        _ => (INTERNAL_SERVER_ERROR.to_string(), "Internal Server Error".to_string())
+    }
+    
+}
+
+// handle_get_request function
+fn handle_get_request(request: &str) -> (String, String) {
+    match (get_id(&request).parse::<i32>, Client::connect(DB_URL, NoTls)) {
+            (Ok(id), Ok(mut client)) => 
+                match client.query("SELECT * FROM users WHERE id = $1", &[&id]) {
+                    Ok(row) => {
+                        let user = User {
+                            id: row.get(0),
+                            name: row.get(1),
+                            email: row.get(2),
+                };
+
+                (OK_RESPONSE.to_string(), serde_json::to_string(&user).unwrap())
+            }
+            _ => (NOT_FOUND.to_string(), "User not found".to_string())
+        }
+        
+        _ => (INTERNAL_SERVER_ERROR.to_string(), "Internal Server Error".to_string())
+    }
+}
+
+// handle_get_all_request function
+fn handle_get_all_request(request: &str) -> (String, String) {
+    match Client::connect(DB_URL, NoTls) {
+        Ok(mut client) => {
+            let mut users = Vec::new();
+            for row in client.query("SELECT * FROM users", &[]).unwrap() {
+                users.push(User {
+                    id: row.get(0),
+                    name: row.get(1),
+                    email: row.get(2),
+                });
+            }
+            (OK_RESPONSE.to_string(), serde_json::to_string(&users).unwrap())
+        }
+        _ => (INTERNAL_SERVER_ERROR.to_string(), "Internal Server Error".to_string())
+    }
+}
+
+//handle_put_request function
+fn handle_put_request(request: &str) -> (String, String) {
+    match (
+        get_id(&request).parse::<i32>, 
+        get_user_request_body(request), 
+        Client::connect(DB_URL, NoTls)
+    ) 
+        {
+        (Ok(id), Ok(user), Ok(mut client)) => {
+            client.execute(
+                "UPDATE users SET name = $1, email = $2 WHERE id = $3",
+                &[&user.name, &user.email, &id]
+            ).unwrap();
+            (OK_RESPONSE.to_string(), "User updated".to_string())
+        }
+        _ => (INTERNAL_SERVER_ERROR.to_string(), "Internal Server Error".to_string())
+    }
+}
+
+//delete_request function
+fn handle_delete_request(request: &str) -> (String, String) {
+    match (get_id(&request).parse::<i32>, Client::connect(DB_URL, NoTls)) {
+        (Ok(id), Ok(mut client)) => {
+            let rows_affected = client.execute("DELETE FROM users WHERE id = $1", &[&id]).unwrap();
+
+            if rows_affected == 0 {
+                return (NOT_FOUND.to_string(), "User not found".to_string());
+            }
+
+            (OK_RESPONSE.to_string(), "User deleted".to_string())
+        }
+        _ => (INTERNAL_SERVER_ERROR.to_string(), "Internal Server Error".to_string())
+    }
+}
+
 
 // set_database function
 fn set_database() -> Resul<(), PostgresError> {
